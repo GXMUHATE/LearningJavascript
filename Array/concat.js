3aec96193faef8806b21975fd47e1645efd97735
Gracane Muhate <gxmuhate@gmail.com>
2022-04-14 15:17:29 +0200

    Concat

---
+++ Array/concat.js
@@ -0,0 +1,6 @@
+const filhas =['Hugo', 'liporto']
+const filhos = ['ferdinande', 'Bioto']
+const todos = filhas.concat(filhos)
+console.log(todos)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6,7]]))
