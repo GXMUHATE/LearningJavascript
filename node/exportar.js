106f346071d16128d86dd869ab404d1b2ed723d9
Gracane Muhate <gxmuhate@gmail.com>
2022-04-29 15:15:14 +0200

    module.exports scope of modification and variations

---
+++ node/exportar.js
@@ -0,0 +1,19 @@
+console.log(module.exports === this)
+console.log(module.exports === exports)
+
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+exports = null
+console.log(module.exports)
+
+exports = {
+    nome: 'Teste'
+}
+
+console.log(module.exports)
+//use module.exports if you want to export a new module content
+module.exports = {
+    publico: true
+}
