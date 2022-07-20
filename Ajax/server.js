99fd04575386794ce5fc9582cc5f04b4c9b02ae8
gmuhate <gracane.muhate@nbf-web.com>
2022-07-20 19:19:25 +0200

    AJAX xmlHTTPRequest 1

--- Ajax/server.js
+++ Ajax/server.js
@@ -6,5 +6,5 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true}))//transform the data sent by a form into objects
 app.use(bodyParser.json())//transform the data sent by json into object
 
-app.get('/teste', (req, res) => res.send('Ok'))
+//app.get('/teste', (req, res) => res.send(new Date)) used just for testing
 app.listen(8080, () => console.log('Executando...'))
