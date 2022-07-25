4f4466cbb8cd2c2959ba3baba6b1146805f82590
gmuhate <gracane.muhate@nbf-web.com>
2022-07-25 19:52:13 +0200

    Webpack - injecting css codes inside javascript file

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,6 +1,7 @@
 // const Pessoa = require('./pessoa')
+// import './modules/moduloA'
 import Pessoa from "./pessoa"
-import './modules/moduloA'
+import './assets/css/estilo.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', //it adds CSS to DOM inecting the tag <style>
+                'css-loader' //interpret @import, url()...
+            ]
+        }]
     }
 }
