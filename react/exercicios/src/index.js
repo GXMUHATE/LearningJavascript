29e121b0714b594ec55ba8c0c60cc342677904e9
gmuhate <gracane.muhate@nbf-web.com>
2022-07-31 17:51:20 +0200

    React - Multiple components

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client';
-import BomDia from './components/BomDia'
+// import BomDia from './components/BomDia'
+import Multi, { BoaNoite } from './components/Multiplos';
 
 //before
 // ReactDOM.render(<BomDia nome="Muhate"/>, document.getElementById('root'))
@@ -10,7 +11,12 @@ import BomDia from './components/BomDia'
 // After
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<BomDia nome="Muhate" />);
+root.render(
+<div>
+<Multi.BoaTarde nome="Muhate" />
+<BoaNoite nome="Graçane" />
+</div>
+);
 
 
 
