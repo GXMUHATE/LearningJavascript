63a1077f696fcd8356c258e930738d2f31723554
gmuhate <gracane.muhate@nbf-web.com>
2022-08-01 16:16:17 +0200

    React - Components of a Class

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client';
+import Saudacao from './components/Saudacao';
 // import BomDia from './components/BomDia'
-import Multi, { BoaNoite } from './components/Multiplos';
+//import Multi, { BoaNoite } from './components/Multiplos';
 
 //before
 // ReactDOM.render(<BomDia nome="Muhate"/>, document.getElementById('root'))
@@ -13,8 +14,7 @@ const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
 <div>
-<Multi.BoaTarde nome="Muhate" />
-<BoaNoite nome="Graçane" />
+<Saudacao tipo="Boa Tarde" nome="Muhate" />
 </div>
 );
 
