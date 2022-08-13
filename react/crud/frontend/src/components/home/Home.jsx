8947dfc87087d2fd47fbe22c3ac31008407f6635
gmuhate <gracane.muhate@nbf-web.com>
2022-08-13 22:20:59 +0200

    Configuring the Main component

---
+++ react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Home"
+    subtitle="Second Project of React Chapter.">
+    <div className='display-4'>Welcome!</div>
+    <hr/>
+    <p className='mb-0'>System to show the building of a Registration developed in React!</p>
+    </Main>
