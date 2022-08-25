329652cf6da856adeefc4a31d1202ce76d810171
gmuhate <gracane.muhate@nbf-web.com>
2022-08-25 17:10:03 +0200

    Fundamentals of Next.Js - Modular css

---
+++ nextJs/cadastroDB(Firestore)/fundamentals/src/pages/estilos.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Estilos.module.css'
+
+export default function Estilos() {
+    return (
+        <div className={styles.skyblue}>
+            <h1>Styling using CSS Modules</h1>
+        </div>
+    )
+}
