5207f510a19f5fdcf6c581fccf3bb0824e19cce2
gmuhate <gracane.muhate@nbf-web.com>
2022-08-25 17:41:24 +0200

    Fundamentals of Next.Js - Components with props

---
+++ nextJs/cadastroDB(Firestore)/fundamentals/src/pages/example.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+export default function Example() {
+    return (
+        <>
+            <Header titulo="Next.Js & React" idade={345} estaBombar={true}/>
+            <Header titulo="The best way of Learning to Code"/>
+        </>
+        
+    )
+}
